8dda7f868a92ec690aef9b67d1b4313e611c368e
Zatanna Lyte <>
2023-07-13 17:15:30 +0300

    s21-day02/wip -> ex03 : add init() and isCorrectFolder() (works correctly)

--- day02/ex03/main.go
+++ day02/ex03/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"os"
+)
+
+var pathToDir *string
+
+func init() {
+	pathToDir = flag.String("a", "", "path to directory")
+	flag.Parse()
+	if err := isCorrectFolder(*pathToDir); *pathToDir != "" && err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 
+}
+
+func isCorrectFolder(dir string) error {
+	if info, err := os.Stat(dir); os.IsNotExist(err) || os.IsPermission(err) || !info.IsDir() {
+		return err
+	}
+	return nil
 }
