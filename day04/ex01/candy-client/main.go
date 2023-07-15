e4823ed29dc57866d7a6cfe89cb6e9c0d2c17580
Zatanna Lyte <>
2023-07-15 14:31:56 +0300

    s21-day04/wip -> ex01 : tls server works, but client cannot request

--- day04/ex01/candy-client/main.go
+++ day04/ex01/candy-client/main.go
@@ -1,5 +1,53 @@
 package main
 
-func main() {
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"day04/ex01/utils"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
 
+const (
+	CertFile = "./tls-cfg/client/cert.pem"
+	KeyFile  = "./tls-cfg/client/key.pem"
+)
+
+func main() {
+	client := getClient()
+	kek := strings.NewReader("`{\"money\": 1, \"candyType\": AA, \"candyCount\": 1}`")
+	resp, err := client.Post("https://localhost:8080/buy_candy", "application/json", kek)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	fmt.Println(resp.Status)
+}
+
+func getClient() *http.Client {
+	data, err := ioutil.ReadFile("./tls-cfg/minica.pem")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	cp, err := x509.SystemCertPool()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	cp.AppendCertsFromPEM(data)
+
+	tlsCfg := &tls.Config{
+		RootCAs:              cp,
+		GetClientCertificate: utils.ClientCertReqFunc(CertFile, KeyFile),
+	}
+
+	return &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: tlsCfg,
+		},
+	}
 }
--- day04/ex01/candy-server/internal/server/server.go
+++ day04/ex01/candy-server/internal/server/server.go
@@ -1,9 +1,19 @@
 package server
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"day04/ex01/candy-server/internal/handler"
+	"day04/ex01/utils"
+	"io/ioutil"
 	"log"
 	"net/http"
+)
+
+const (
+	CertFile = "./tls-cfg/localhost/cert.pem"
+	KeyFile  = "./tls-cfg/localhost/key.pem"
+	MiniCA   = "./tls-cfg/minica.pem"
 )
 
 type Server struct {
@@ -11,17 +21,36 @@ type Server struct {
 }
 
 func NewServer() *Server {
+	data, err := ioutil.ReadFile(MiniCA)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	cp, err := x509.SystemCertPool()
+	if err != nil {
+		log.Println(err)
+	}
+
+	cp.AppendCertsFromPEM(data)
+
+	tlsCfg := &tls.Config{
+		ClientCAs:      cp,
+		ClientAuth:     tls.RequireAndVerifyClientCert,
+		GetCertificate: utils.CertReqFunc(CertFile, KeyFile),
+	}
+
 	return &Server{
 		httpServer: http.Server{
-			Addr:    "127.0.0.1:8080",
-			Handler: &handler.Handler{},
+			Addr:      ":8080",
+			TLSConfig: tlsCfg,
+			Handler:   &handler.Handler{},
 		},
 	}
 }
 
 func (s *Server) Run() error {
 	log.Println("server is running")
-	return s.httpServer.ListenAndServeTLS("./tls-cfg/localhost/", "")
+	return s.httpServer.ListenAndServeTLS("", "")
 }
 
 func (s *Server) Stop() error {
