65e2c64d2feee698eb0c7937a50fb79f53074633
Zatanna Lyte <>
2023-07-17 20:09:34 +0300

    s21-day06/wip -> ex02 : add /admin endpoint, add html generating, add check for login and password

---
+++ day06/ex01/cmd/myblog/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type AdminContent struct {
+	Login         string
+	Password      string
+	Clicked       bool
+	StorageAccess string
+}
+
+type AdminCredentials struct {
+	AdminLogin    string
+	AdminPassword string
+	DBName        string
+	DBTable       string
+}
+
+var (
+	ac   AdminCredentials
+	tmpl = template.Must(template.ParseFiles("./ui/html/admin.html"))
+)
+
+func init() {
+	if err := getDataFromCredentials(); err != nil {
+		log.Println(err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/admin", handle)
+	log.Println("Server started ...")
+	log.Fatalln(http.ListenAndServe("localhost:8080", nil))
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	data := AdminContent{
+		Login:    r.FormValue("login"),
+		Password: r.FormValue("password"),
+	}
+
+	tmpl.Execute(w, data)
+	if validateAdmin(data.Login, data.Password) {
+		log.Println("success")
+	} else {
+		log.Println("not success")
+	}
+}
+
+func validateAdmin(login, password string) bool {
+	if login == ac.AdminLogin && password == ac.AdminPassword {
+		return true
+	}
+	return false
+}
+
+func getDataFromCredentials() error {
+	file, err := os.Open("./internal/admin_credentials.txt")
+	if err != nil {
+		return err
+	}
+	reader := bufio.NewScanner(file)
+	for reader.Scan() {
+		line := reader.Text()
+		credentials := strings.Split(line, ":")
+		switch credentials[0] {
+		case "adminLogin":
+			ac.AdminLogin = credentials[1]
+		case "adminPassword":
+			ac.AdminPassword = credentials[1]
+		case "dbName":
+			ac.DBName = credentials[1]
+		case "dbTable":
+			ac.DBTable = credentials[1]
+		default:
+			return errors.New("error in parsing admin credentials")
+		}
+	}
+	return nil
+}
