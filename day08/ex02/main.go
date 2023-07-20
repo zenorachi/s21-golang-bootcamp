741295024928e49a3f5e66c9b5b1a4d45120e3ed
Zatanna Lyte <>
2023-07-20 20:32:20 +0300

    s21-day08/finish -> ex02 : probably done

--- day08/ex02/main.go
+++ day08/ex02/main.go
@@ -1,6 +1,27 @@
 package main
 
+// #cgo CFLAGS: -x objective-c
+// #cgo LDFLAGS: -framework Cocoa
+// #include "application.h"
+// #include "window.h"
 import "C"
+import (
+	"github.com/go-vgo/robotgo"
+	"unsafe"
+)
 
 func main() {
+	C.InitApplication()
+
+	title := C.CString("School 21")
+
+	screenWidth, screenHeight := robotgo.GetScreenSize()
+	cWidth := C.int(screenWidth)
+	cHeight := C.int(screenHeight)
+
+	wPtr := C.Window_Create(cWidth/2, cHeight/2, 300, 200, title)
+	C.Window_MakeKeyAndOrderFront(wPtr)
+
+	defer C.free(unsafe.Pointer(title))
+	C.RunApplication()
 }
